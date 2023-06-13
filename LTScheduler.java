aa9f98f9e455ba31b28a8f4841b77fa603efed38
OverMan-647 <brunoadji@id.uff.br>
2023-06-13 18:24:31 -0300

    Adding space

--- LTScheduler.java
+++ LTScheduler.java
@@ -1,6 +1,6 @@
 package simulador_escalonador;
 
-//Escalonador de longo prazo precisa acessar as filas e colocar um processo novo na fila de prontos
+// Escalonador de longo prazo precisa acessar as filas e colocar um processo novo na fila de prontos
 // O SO vai acessá-lo, passar o novo processo como parametro e ele então pode o escalonar
 
 public class LTScheduler {
