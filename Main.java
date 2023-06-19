58bb45773584c5871a906a5cdf296df66c6d15ba
OverMan-647 <brunoadji@id.uff.br>
2023-06-19 01:55:54 -0300

    Removing unecessary comments and imports

--- Main.java
+++ Main.java
@@ -1,8 +1,5 @@
 package simulador_escalonador;
 
-import java.util.LinkedList;
-import java.util.Queue;
-
 public class Main {
     
     public static void main(String[] args) {
@@ -11,17 +8,6 @@ public class Main {
         os.createProcess("P1",0, 0, 10, 2504, 0, 4, 6);
 
         os.executeCicle();
-        /*
-        Queue<ProcessOS> priorityQueue = new LinkedList<ProcessOS>();
-        priorityQueue.add(new ProcessOS("P1",0, 0, 10, 2504, 0, 4, 6));
-
-        for (ProcessOS processOS : priorityQueue) {
-                processOS.print();;
-        }
-
-        for (ProcessOS processOS : priorityQueue) {
-                processOS.print();;
-        }*/
 
         os.print();
     }
